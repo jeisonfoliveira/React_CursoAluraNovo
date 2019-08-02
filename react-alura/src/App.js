0b684ec4187dde4b25afa2547b47efd886fff3e7
Oliveira <jeison.f.oliveira@accenture.com>
2019-08-02 11:15:21 -0300

    commit projeto

---
+++ react-alura/src/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Tabela from './Tabela';
+import Formulario from './Formulario';
+
+class App extends Component {
+
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ],
+  }
+
+  removeAutor = index => {
+    const{autores} = this.state;
+
+    this.setState({
+      autores: autores.filter((autor,posAtual) => {
+        return posAtual !== index;
+      }),
+    });
+  }
+
+  escutadorDeSubmit = autor => {
+    this.setState({autores: [...this.state.autores, autor]});
+  }
+
+  render(){
+    return(
+      <div>
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+        <Formulario escutadorDeSubmit={this.escutadorDeSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
